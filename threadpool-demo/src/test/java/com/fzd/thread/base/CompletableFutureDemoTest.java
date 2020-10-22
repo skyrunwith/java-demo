4b8a286d9742ed62ecbfd8bcb747f5009cb1b98f
skyrunwith <582547045@qq.com>
2020-10-22 20:31:49 +0800

    新增 Completable Future 显示完成方法

--- threadpool-demo/src/test/java/com/fzd/thread/base/CompletableFutureDemoTest.java
+++ threadpool-demo/src/test/java/com/fzd/thread/base/CompletableFutureDemoTest.java
@@ -2,9 +2,11 @@ package com.fzd.thread.base;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
+import sun.util.locale.provider.AvailableLanguageTags;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * 功能描述:
@@ -14,6 +16,49 @@ import java.util.concurrent.TimeUnit;
  */
 @Log4j2
 public class CompletableFutureDemoTest {
+
+    /**
+     * 主动完成任务
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Test
+    public void completeApi() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                log.info("begin runAsync");
+                TimeUnit.SECONDS.sleep(3);
+                log.info("end runAsync");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "auto complete";
+        });
+        TimeUnit.SECONDS.sleep(2);
+        assertTrue(f1.complete("manual compete"));
+        log.info(f1.get());
+    }
+
+    @Test
+    public void completeApi2() throws ExecutionException, InterruptedException, TimeoutException {
+        final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+        final ThreadPoolExecutor pool = new ThreadPoolExecutor(AVAILABLE_PROCESSORS, AVAILABLE_PROCESSORS * 2
+                , 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(5), new ThreadPoolExecutor.CallerRunsPolicy());
+        CompletableFuture<String> future = new CompletableFuture<>();
+        pool.execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(Thread.currentThread().getName() + " set future result");
+//            future.complete("hello, fzd"); //如果不手动，完成，则一直阻塞
+        });
+        log.info("main thread wait future result");
+        log.info(future.get()); // 调用get， future 会被激活
+//        log.info(future.get(1, TimeUnit.SECONDS)); // TimeoutException 异常
+        log.info("main thread got future result");
+    }
 
     /**
      * CompletableFuture 完成泡茶程序
