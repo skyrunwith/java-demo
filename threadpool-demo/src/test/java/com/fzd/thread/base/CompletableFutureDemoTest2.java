894ffd4a30eed6e7119a17f73732bf34b3d6d84b
付祖德 <skyrunwith>
2020-10-23 00:01:21 +0800

    CompletableFuture Demo

---
+++ threadpool-demo/src/test/java/com/fzd/thread/base/CompletableFutureDemoTest2.java
@@ -0,0 +1,83 @@
+package com.fzd.thread.base;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * CompletableFuture Demo
+ *  thenRun、thenAccept、thenApply、whenComplete
+ */
+@Log4j2
+public class CompletableFutureDemoTest2 {
+    final ThreadPoolExecutor pool = new ThreadPoolExecutor(4, 8, 1, TimeUnit.MINUTES
+            , new LinkedBlockingQueue<>(5), new ThreadPoolExecutor.CallerRunsPolicy());
+
+    /**
+     * thenRun 语法测试，Async线程切换测试，只有thenRunAsync传了线程池参数才会切换
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void thenRun() throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(Thread.currentThread().getName() + " future done");
+        });
+        CompletableFuture<Void> future2 = future.thenRun(() -> {
+            log.info(Thread.currentThread().getName() + " future2 done");
+        });
+        CompletableFuture<Void> future3 = future2.thenRunAsync(() -> {
+            log.info(Thread.currentThread().getName() + " future3 done");
+        });
+        // 线程池中运行，其他都是在同一线程中运行
+        CompletableFuture<Void> future4 = future3.thenRunAsync(() -> {
+            log.info(Thread.currentThread().getName() + " future4 done");
+        }, pool);
+        CompletableFuture<Void> future5 = future4.thenRunAsync(() -> {
+            log.info(Thread.currentThread().getName() + " future5 done");
+        });
+        future5.get();
+        log.info("main thread got result");
+    }
+
+    @Test
+    public void thenAccept() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> Thread.currentThread().getName() + " future done");
+        CompletableFuture<Void> future2 = future.thenAcceptAsync(str -> {
+            log.info(str);
+            log.info(Thread.currentThread().getName() + " future2 done");
+        });
+        future2.get();
+        log.info("main thread got result");
+    }
+
+    @Test
+    public void thenApply() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> Thread.currentThread().getName() + " future done");
+        CompletableFuture<String> future2 = future.thenApplyAsync(str -> {
+            log.info(str);
+            return Thread.currentThread().getName() + " future2 done";
+        });
+        log.info(future2.get());
+    }
+
+    @Test
+    public void whenComplete() throws InterruptedException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> Thread.currentThread().getName() + "future done");
+        future.whenCompleteAsync((str, t) -> {
+            if(t == null){
+                log.info(str);
+            }else {
+                t.printStackTrace();
+            }
+            log.info(Thread.currentThread().getName() + "complete");
+        }, pool);
+        Thread.currentThread().join();
+    }
+}
---
+++ threadpool-demo/src/test/java/com/fzd/thread/base/CompletableFutureDemoTest3.java
@@ -0,0 +1,130 @@
+package com.fzd.thread.base;
+
+import javafx.geometry.VPos;
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CompletableFuture Demo
+ * thenCompose
+ */
+@Log4j2
+public class CompletableFutureDemoTest3 {
+
+    /**
+     * 一个CompleteFuture完成后执行另一个CompleteFuture
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void thenCompose() throws ExecutionException, InterruptedException {
+        // 同一个线程中执行
+//        CompletableFuture<String> str = doSomeThingOne("123").thenCompose(this::doSomeThingTwo);
+        // 不同线程中执行
+        CompletableFuture<String> str = doSomeThingOne("123").thenComposeAsync(this::doSomeThingTwo);
+        log.info(str.get());
+    }
+
+    /**
+     * 两个并行的CompleteFuture完成后，再调用BiFunction
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void thenCombine() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> str = doSomeThingOne("123").thenCombineAsync(doSomeThingTwo("456"),
+                (x, y) -> x + y);
+        log.info(str.get());
+    }
+
+    /**
+     * 多个并行的 CompleteFuture 完成
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void allOf() throws ExecutionException, InterruptedException {
+        // future 运行在不同的线程
+        List<CompletableFuture<String>> futureList = new ArrayList<>();
+        futureList.add(doSomeThingOne("1"));
+        futureList.add(doSomeThingOne("2"));
+        futureList.add(doSomeThingOne("3"));
+        futureList.add(doSomeThingOne("4"));
+        CompletableFuture<Void> result = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[1]));
+        // 等待所有任务完成
+        log.info(result.get());
+    }
+
+    /**
+     * 任一一个并行的CompletableFuture 完成
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void anyOf() throws ExecutionException, InterruptedException {
+        // future 运行在不同的线程
+        List<CompletableFuture<String>> futureList = new ArrayList<>();
+        futureList.add(doSomeThingOne("1"));
+        futureList.add(doSomeThingOne("2"));
+        futureList.add(doSomeThingTwo("3"));
+        CompletableFuture<Object> result = CompletableFuture.anyOf(futureList.toArray(new CompletableFuture[1]));
+        // 等待其中一个任务完成
+        log.info(result.get());
+    }
+
+    /**
+     * 异常处理
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void whenCompleteException() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                if(true) {
+                    throw new RuntimeException("exception test");
+                }
+                future.complete("future complete");
+            }catch (Exception e){
+                // 未添加该代码会一直阻塞
+                future.completeExceptionally(e);
+            }
+        }, "thread-1").start();
+        log.info(future.exceptionally(t -> "default").get());
+    }
+
+    CompletableFuture<String> doSomeThingOne(String encodeCompanyId){
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String id = encodeCompanyId;
+            log.info(Thread.currentThread().getName() + " doSomeThing one done");
+            return id;
+        });
+    }
+
+    CompletableFuture<String> doSomeThingTwo(String companyId){
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String id = companyId + ":bdfus";
+            log.info(Thread.currentThread().getName() + " doSomeThing two done");
+            return id;
+        });
+    }
+}
